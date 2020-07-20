45d678e672bf6e381d26c7f81c0dfd96cc2d49fa
Alessio <barrialex@gmail.com>
2020-07-20 11:16:22 +0200

    gh pages path

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,7 +1,7 @@
 import { Product } from './product';
 export const PRODUCTS: Product[] = [
-  { id: 1, name: "S'incantu", imageUrl: '../assets/images/casizolu.jpg' },
-  { id: 2, name: 'Margara', imageUrl: '../assets/images/margara2.jpg' },
-  { id: 3, name: 'Yogurt', imageUrl: '../assets/images/yogurt.jpg' },
-  { id: 4, name: 'Fresina', imageUrl: '../assets/images/fresina.jpg' },
+  { id: 1, name: "S'incantu", imageUrl: './assets/images/casizolu.jpg' },
+  { id: 2, name: 'Margara', imageUrl: './assets/images/margara2.jpg' },
+  { id: 3, name: 'Yogurt', imageUrl: './assets/images/yogurt.jpg' },
+  { id: 4, name: 'Fresina', imageUrl: './assets/images/fresina.jpg' },
 ];
